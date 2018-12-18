1413199b5c616d68ad7fc10266237ce0612118f9
BilalyC <bilaly.camara@outlook.com>
2018-12-18 14:03:07 +0100

    Exercice 2 Complet

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] week = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
+            Console.WriteLine(week[1]);
+            Console.WriteLine(week[4]);
+            //Console.WriteLine(week[1] + "\n" + week[4]);
+        }
+    }
+}
